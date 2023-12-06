fba21915266a4645f47af163b0c7430044977317
xTHANATx <thanat@ktc.co.th>
2023-12-06 14:30:19 +0700

    Web App 2 Url

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("สวัสดี");
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/register', function (req, res) {
+    res.send('Register page')
+  })
+
+app.listen(3000)
